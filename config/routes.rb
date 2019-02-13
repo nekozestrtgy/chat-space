49ee07b4134ba0c73bcae0e2f21499d599c509d3
nekozestrtgy <nekozestrtgy@gmail.com>
2019-02-13 21:46:38 +0900

    Setup-group-function
    グループ編集・作成機能を実装した。

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root 'messages#index'
+  root 'groups#index'
 
   get 'users/edit'
 
   get 'users/update'
+
+  resources :uses, only: [:index, :edit, :update]
+
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
